2fbc8f536f7c20f872c75831bb3bc8a252ff1058
Pentti Laitinen <lana@lana-laptop.(none)>
2009-02-05 22:16:45 +0800

    Added directory checking
    Signed-off-by: fooga <kai.kousa@gmail.com>

--- video2json/app/controllers/json_controller.rb
+++ video2json/app/controllers/json_controller.rb
@@ -4,6 +4,12 @@ class JsonController < ApplicationController
   attr_accessor :lib
 
   def index
+    if ! File.exist?("#{RAILS_ROOT}/videos")
+                    File.makedirs ("#{RAILS_ROOT}/videos")
+    end
+    if ! File.exist?("#{RAILS_ROOT}/audio")
+                    File.makedirs ("#{RAILS_ROOT}/audio")
+    end
   end
 
   def list
