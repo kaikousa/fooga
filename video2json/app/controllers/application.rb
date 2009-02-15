bea07ca77fc36cacfa41d6c592511365ba1e20da
Kai Kousa <kai.kousa@gmail.com>
2009-02-15 18:12:46 +0200

    Applied changes for VEE integration

--- video2json/app/controllers/application.rb
+++ video2json/app/controllers/application.rb
@@ -3,7 +3,7 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-
+  
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '93c19daaf584e6b205b173cd0a323b14'
--- video2json/app/controllers/json_controller.rb
+++ video2json/app/controllers/json_controller.rb
@@ -1,7 +1,8 @@
 class JsonController < ApplicationController
   require 'json'
   require 'ftools'
-
+  require "model/audio"
+  
   attr_accessor :lib
 
   def index
@@ -11,12 +12,17 @@ class JsonController < ApplicationController
     if ! File.exist?("#{RAILS_ROOT}/audio")
                     File.makedirs ("#{RAILS_ROOT}/audio")
     end
+    
     list
   end
 
   def list
     data = Media.new
     @lib = data.list
+  end
+  
+  def test_vee
+    @audio = Audio.new("ghosts.mp3", "", "", "", [])
   end
 
 end
